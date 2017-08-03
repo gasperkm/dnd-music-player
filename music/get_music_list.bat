4f48ff3625fa05fa311070ddf94ec46a93399111
Gasper Kukec <gasperkm@gmail.com>
2017-08-03 18:27:16 +0200

    Updated functions
    Updated functions for player and added possibility to import a preset defined from the subfolders of the music folder.

--- music/get_music_list.bat
+++ music/get_music_list.bat
@@ -1,28 +1,79 @@
 @ECHO OFF
-REM These are comments
-echo Fetching a list of files in each music subfolder...
-break > .\ambient_list.txt
-break > .\battle_list.txt
-break > .\creepy_list.txt
-break > .\tavern_list.txt
-break > .\temple_list.txt
-break > .\town_list.txt
-REM Append any additional subfolder lists, if new music subfolder is made
+REM Enable nested for loops
+SetLocal EnableDelayedExpansion
 
-echo Checking .\ambient subfolder
-for /r . %%g in (.\ambient\*) do echo %%~nxg >> .\ambient_list.txt
-echo Checking .\battle subfolder
-for /r . %%g in (.\battle\*) do echo %%~nxg >> .\battle_list.txt
-echo Checking .\creepy subfolder
-for /r . %%g in (.\creepy\*) do echo %%~nxg >> .\creepy_list.txt
-echo Checking .\tavern subfolder
-for /r . %%g in (.\tavern\*) do echo %%~nxg >> .\tavern_list.txt
-echo Checking .\temple subfolder
-for /r . %%g in (.\temple\*) do echo %%~nxg >> .\temple_list.txt
-echo Checking .\town subfolder
-for /r . %%g in (.\town\*) do echo %%~nxg >> .\town_list.txt
+echo Fetching lists of files in each music subfolder...
+break > .\music_library.js
+
+set /a counter=0
+
+REM Append any additional subfolder lists
+echo|set /p="var musiclist = [" >> .\music_library.js
+for /D %%i in (*.*) do (
+  if !counter! == 0 (
+    set /a counter=counter+1
+    echo Checking %%i subfolder
+    echo|set /p="[" >> .\music_library.js
+REM    echo|set /p=""subfolder_%%i"" >> .\music_library.js
+    echo|set /p=""%%i"" >> .\music_library.js
+    for /r . %%g in (.\%%i\*) do echo|set /p=", "%%i/%%~nxg"" >> .\music_library.js
+    echo|set /p="]" >> .\music_library.js
+  ) else (
+    echo , >> .\music_library.js
+    set /a counter=counter+1
+    echo Checking %%i subfolder
+    echo|set /p="[" >> .\music_library.js
+    echo|set /p=""%%i"" >> .\music_library.js
+    for /r . %%g in (.\%%i\*) do echo|set /p=", "%%i/%%~nxg"" >> .\music_library.js
+    echo|set /p="]" >> .\music_library.js
+  )
+)
+echo ]; >> .\music_library.js
+
+echo var nrlibrary = !counter!; >> .\music_library.js
+
+
+REM REM Append any additional subfolder lists
+REM for /D %%i in (*.*) do (
+REM   set /a counter=counter+1
+REM   echo Checking %%i subfolder
+REM   echo|set /p="var list!counter! = [" >> .\music_library.js
+REM   echo|set /p=""subfolder_%%i"" >> .\music_library.js
+REM   for /r . %%g in (.\%%i\*) do echo|set /p=", "%%i/%%~nxg"" >> .\music_library.js
+REM   echo ]; >> .\music_library.js
+REM )
+REM 
+REM echo var nrlibrary = !counter!; >> .\music_library.js
 
 goto EXIT
 :EXIT
-ECHO Press any key to exit...
+echo Press any key to exit...
 pause>null
+
+REM REM These are comments
+REM echo Fetching a list of files in each music subfolder...
+REM break > .\ambient_list.txt
+REM break > .\battle_list.txt
+REM break > .\creepy_list.txt
+REM break > .\tavern_list.txt
+REM break > .\temple_list.txt
+REM break > .\town_list.txt
+REM REM Append any additional subfolder lists, if new music subfolder is made
+REM 
+REM echo Checking .\ambient subfolder
+REM for /r . %%g in (.\ambient\*) do echo %%~nxg >> .\ambient_list.txt
+REM echo Checking .\battle subfolder
+REM for /r . %%g in (.\battle\*) do echo %%~nxg >> .\battle_list.txt
+REM echo Checking .\creepy subfolder
+REM for /r . %%g in (.\creepy\*) do echo %%~nxg >> .\creepy_list.txt
+REM echo Checking .\tavern subfolder
+REM for /r . %%g in (.\tavern\*) do echo %%~nxg >> .\tavern_list.txt
+REM echo Checking .\temple subfolder
+REM for /r . %%g in (.\temple\*) do echo %%~nxg >> .\temple_list.txt
+REM echo Checking .\town subfolder
+REM for /r . %%g in (.\town\*) do echo %%~nxg >> .\town_list.txt
+REM 
+REM goto EXIT
+REM :EXIT
+REM ECHO Press any key to exit...
+REM pause>null
--- music/get_music_list.sh
+++ music/get_music_list.sh
@@ -1,46 +1,103 @@
 #!/bin/bash
 
 echo "Fetching a list of files in each music subfolder..."
-echo -n "" > ambient_list.txt
-echo -n "" > battle_list.txt
-echo -n "" > creepy_list.txt
-echo -n "" > tavern_list.txt
-echo -n "" > temple_list.txt
-echo -n "" > town_list.txt
-# Append any additional subfolder lists, if new music subfolder is made
+echo -n "" > ./music_library.js
 
-echo "Checking ./ambient subfolder"
-for i in $(ls ./ambient/*)
+counter=0
+
+# Append any additional subfolder lists
+echo -n "var musiclist = [" >> ./music_library.js
+for d in ./*/
 do
-   echo $(basename $i) >> ./ambient_list.txt
+   if [ $counter == 0 ]; then
+      counter=$((counter+1))
+      echo "Checking $d subfolder"
+	  echo -n "[" >> ./music_library.js
+#      echo -n "\"subfolder_$(basename $d)\"" >> ./music_library.js
+      echo -n "\"$(basename $d)\"" >> ./music_library.js
+      for i in ./$d/*
+      do
+         echo -n ", \"${d}$(basename $i)\"" >> ./music_library.js
+      done
+	  echo -n "]" >> ./music_library.js
+   else
+      echo "," >> ./music_library.js
+      counter=$((counter+1))
+      echo "Checking $d subfolder"
+	  echo -n "[" >> ./music_library.js
+#      echo -n "\"subfolder_$(basename $d)\"" >> ./music_library.js
+      echo -n "\"$(basename $d)\"" >> ./music_library.js
+      for i in ./$d/*
+      do
+         echo -n ", \"${d}$(basename $i)\"" >> ./music_library.js
+      done
+	  echo -n "]" >> ./music_library.js
+   fi
 done
-echo "Checking ./battle subfolder"
-for i in $(ls ./battle/*)
-do
-   echo $(basename $i) >> ./battle_list.txt
-done
-echo "Checking ./creepy subfolder"
-for i in $(ls ./creepy/*)
-do
-   echo $(basename $i) >> ./creepy_list.txt
-done
-echo "Checking ./tavern subfolder"
-for i in $(ls ./tavern/*l)
-do
-   echo $(basename $i) >> ./tavern_list.txt
-done
-echo "Checking ./temple subfolder"
-for i in $(ls ./temple/*)
-do
-   echo $(basename $i) >> ./temple_list.txt
-done
-echo "Checking ./town subfolder"
-for i in $(ls ./town/*)
-do
-   echo $(basename $i) >> ./town_list.txt
-done
+echo "];" >> ./music_library.js
+
+echo "var nrlibrary = $counter;" >> ./music_library.js
+
+#for d in ./*/
+#do
+#   counter=$((counter+1))
+#   echo "Checking $d subfolder"
+#   echo -n "var list$counter = [" >> ./music_library.js
+#   echo -n "\"subfolder_$(basename $d)\"" >> ./music_library.js
+#   for i in ./$d/*
+#   do
+#      echo -n ", \"${d}$(basename $i)\"" >> ./music_library.js
+#   done
+#   echo "];" >> ./music_library.js
+#done
+#
+#echo "var nrlibrary = $counter;" >> ./music_library.js
 
 read -n 1 -s -p "Press any key to exit..."
 echo ""
-
 exit 0
+
+#echo "Fetching a list of files in each music subfolder..."
+#echo -n "" > ambient_list.txt
+#echo -n "" > battle_list.txt
+#echo -n "" > creepy_list.txt
+#echo -n "" > tavern_list.txt
+#echo -n "" > temple_list.txt
+#echo -n "" > town_list.txt
+## Append any additional subfolder lists, if new music subfolder is made
+#
+#echo "Checking ./ambient subfolder"
+#for i in $(ls ./ambient/*)
+#do
+#   echo $(basename $i) >> ./ambient_list.txt
+#done
+#echo "Checking ./battle subfolder"
+#for i in $(ls ./battle/*)
+#do
+#   echo $(basename $i) >> ./battle_list.txt
+#done
+#echo "Checking ./creepy subfolder"
+#for i in $(ls ./creepy/*)
+#do
+#   echo $(basename $i) >> ./creepy_list.txt
+#done
+#echo "Checking ./tavern subfolder"
+#for i in $(ls ./tavern/*l)
+#do
+#   echo $(basename $i) >> ./tavern_list.txt
+#done
+#echo "Checking ./temple subfolder"
+#for i in $(ls ./temple/*)
+#do
+#   echo $(basename $i) >> ./temple_list.txt
+#done
+#echo "Checking ./town subfolder"
+#for i in $(ls ./town/*)
+#do
+#   echo $(basename $i) >> ./town_list.txt
+#done
+#
+#read -n 1 -s -p "Press any key to exit..."
+#echo ""
+#
+#exit 0
